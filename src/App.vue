7c0c32759fbf0a2a5ab7436b1a0642ac3a275358
hommaryota <ryo4111016@gmail.com>
2024-09-23 23:11:25 +0900

    Todoリスト実装

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,123 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import Button from './components/button/Button.vue'
+
+type Todos = {
+  id: number
+  text: string
+  done: boolean
+}[]
+
+let id = 0
+const text = ref('')
+const todos = ref<Todos>([])
+
+const addTodo = () => {
+  const newTodo = {
+    id: id++,
+    text: text.value,
+    done: false
+  }
+  todos.value.push(newTodo)
+  text.value = ''
+}
+
+const deleteTodo = (i: number) => {
+  todos.value = todos.value.filter((todo) => todo.id !== todos.value[i].id)
+}
+
+const confirmed = (index: number) => {
+  todos.value[index].done = !todos.value[index].done
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <h1 class="title">今日やること</h1>
+    <div class="input-warp">
+      <input type="text" class="input" v-model="text" />
+      <Button
+        @click="addTodo"
+        :color="text ? '#008CFF' : '#c1c1c1'"
+        :isDisable="!text"
+        :cursor="!!text"
+        >追加</Button
+      >
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <ul class="todo-ul">
+      <li class="todo-wrap" v-for="(todo, index) in todos" :key="todo.id">
+        <span class="todo" :class="todo.done && 'confirmedTodo'">{{ todo.text }}</span>
+        <Button color="#FF3700" @click="deleteTodo(index)">削除</Button>
+        <Button
+          @click="confirmed(index)"
+          :color="todo.done ? '' : '#008CFF'"
+          :text-decoration="todo.done ? 'line-through' : ''"
+          >完了</Button
+        >
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  width: 375px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  translate: -50% -50%;
+  background: #ffffff;
+  box-shadow: 0px 6px 6px #0000001a;
+  border-radius: 24px;
+  padding: 24px;
+  box-sizing: border-box;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.title {
+  text-align: left;
+  font: normal normal normal 24px/41px Hiragino Maru Gothic ProN;
+  letter-spacing: 0px;
+  color: #000000;
+  margin-bottom: 32px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.input-warp {
+  display: flex;
+  gap: 20px;
+  margin-bottom: 32px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.input {
+  width: 250px;
+  height: 32px;
+  background: #ffffff;
+  border: 1px solid #cdcdcd;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.todo-ul {
+  margin: 0;
+  padding: 0;
+}
+
+.todo-wrap {
+  display: flex;
+  gap: 20px;
+  padding-bottom: 8px;
+  border-bottom: 1px solid #cdcdcd;
+}
+
+.todo-wrap:not(:first-child) {
+  margin-top: 16px;
+}
+
+.todo {
+  flex: 1;
+}
+
+.confirmedTodo {
+  text-decoration: line-through;
 }
 </style>
---
+++ src/components/button/Button.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { defineProps, toRefs, defineEmits } from 'vue'
+const props = defineProps({
+  isDisable: { type: Boolean, default: false },
+  color: { type: String, required: true },
+  cursor: { type: Boolean, required: false }
+})
+const { isDisable, color } = toRefs(props)
+const emit = defineEmits<{
+  (e: 'click'): void
+}>()
+</script>
+
+<template>
+  <button
+    class="button"
+    :style="{ backgroundColor: color, cursor: cursor ? 'pointer' : 'initial' }"
+    @click="emit('click')"
+    :disabled="isDisable"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  width: 57px;
+  height: 32px;
+  background: #c1c1c1;
+  border-radius: 4px;
+  border: none;
+  font: normal normal normal 14px/24px Hiragino Maru Gothic ProN;
+  letter-spacing: 0px;
+  color: #ffffff;
+}
+</style>
